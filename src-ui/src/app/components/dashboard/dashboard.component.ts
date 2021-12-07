b6e6e196cc1920b9f0e946b564e65db8ff5a7fc8
yckwan <kwanyc95@outlook.com>
2021-12-07 22:12:11 +0800

    update contents

--- src-ui/src/app/components/dashboard/dashboard.component.ts
+++ src-ui/src/app/components/dashboard/dashboard.component.ts
@@ -30,9 +30,9 @@ export class DashboardComponent implements OnInit {
 
   get subtitle() {
     if (this.displayName) {
-      return $localize`Hello ${this.displayName}, welcome to Paperless-ng!`
+      return $localize`Hello ${this.displayName}, welcome to Document Management Systems!`
     } else {
-      return $localize`Welcome to Paperless-ng!`
+      return $localize`Welcome to Document Management Systems!`
     }
   }
 
